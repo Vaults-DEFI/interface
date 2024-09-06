c1e32bf4563b9136ebc5bce077c9a5c89cf19db2
isha-bagadiya <ishabagadiya@lampos.tech>
2024-09-06 15:58:23 +0530

    formatted

--- app/wagmi.js
+++ app/wagmi.js
@@ -2,32 +2,31 @@ import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { rootstock } from "viem/chains";
 // import { rootstockTestnet } from "wagmi/chains";
 
-const localhost = {
-  id: 30,
-  name: 'Localhost',
-  network: 'localhost',
-  nativeCurrency: {
-    name: 'RBTC',
-    symbol: 'RBTC',
-    decimals: 18,
-  },
-  rpcUrls: {
-    default: {
-      http: ['http://127.0.0.1:8545'], 
-    },
-    public: {
-      http: ['http://127.0.0.1:8545'], 
-    },
-  },
-  testnet: true,
-};
+// const localhost = {
+//   id: 30,
+//   name: 'Localhost',
+//   network: 'localhost',
+//   nativeCurrency: {
+//     name: 'RBTC',
+//     symbol: 'RBTC',
+//     decimals: 18,
+//   },
+//   rpcUrls: {
+//     default: {
+//       http: ['http://127.0.0.1:8545'], 
+//     },
+//     public: {
+//       http: ['http://127.0.0.1:8545'], 
+//     },
+//   },
+//   testnet: true,
+// };
 
 export const config = getDefaultConfig({
   appName: "vaults",
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   chains: [
     rootstock,
-    localhost, 
     // rootstockTestnet,
   ],
   ssr: true,
